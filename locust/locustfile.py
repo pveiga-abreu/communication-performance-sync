e7264f5818295694ebabab0246a87f2a8faf1e91
pveiga-abreu <pveiga.abreu@gmail.com>
2025-03-16 20:44:37 -0300

    Add Locust service to Docker Compose and update locustfile for order creation testing

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,26 +1,10 @@
 import random
-from locust import HttpUser, task, tag
-import requests
+
+from locust import HttpUser, tag, task
+
 
 class User(HttpUser):
-    
-    def _create_order(self):
-        customer_id = str(random.randint(1, 1000))
-        total_amount = random.randint(100, 1000)
-        items = [f"item{random.randint(1, 99)}" for i in range(1, 5)]
-
-        response = requests.post(
-            "/create_order",
-            json={
-                "customer_id": customer_id,
-                "items": items,
-                "total_amount": total_amount,
-            },
-        )
-
-        return response.json()["order_id"]
-
-    @tag('create')
+    @tag("create")
     @task
     def create_order(self):
         customer_id = str(random.randint(1, 1000))
@@ -35,9 +19,3 @@ class User(HttpUser):
                 "total_amount": total_amount,
             },
         )
-
-    @tag('get')
-    @task
-    def get_order(self):
-        order_id = self._create_order()
-        self.client.get(f"/orders/{order_id}")
